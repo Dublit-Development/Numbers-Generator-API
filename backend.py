d7c879d0951a8801f2ab94a6e5a11f191581dfc0
Joseph <joemenjr@gmail.com>
2023-11-24 16:26:32 -0500

    limit updated

--- backend.py
+++ backend.py
@@ -4,7 +4,11 @@ from flask_limiter.util import get_remote_address
 from flask import make_response
 
 app = Flask(__name__)
-limiter = Limiter(app, key_func=get_remote_address)
+limiter = Limiter(
+    get_remote_address,
+    app=app,
+    default_limits=["200 per day", "50 per hour"]
+)
 
 @app.route('/')
 def index():
