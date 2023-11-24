7da6e864fcca692f73fce2291f50936ab5ed8f6f
Joseph <joemenjr@gmail.com>
2023-11-24 16:18:59 -0500

    Added Rate Limit

--- README.md
+++ README.md
@@ -10,3 +10,5 @@ sudo apt-get install python3-pip
 # The following libraries need to be installed using pip:
 
 pip install Flask requests
+
+pip install Flask-Limiter
--- backend.py
+++ backend.py
@@ -1,16 +1,38 @@
 from flask import Flask, render_template, jsonify
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+from flask import make_response
 
 app = Flask(__name__)
+limiter = Limiter(
+    app,
+    key_func=get_remote_address,
+    default_limits=["2 per second"]
+)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/generate_numbers', methods=['POST'])
+@limiter.limit("2 per second")
 def generate_numbers():
     # Process the request and send random numbers back
     import random
     numbers = [random.randint(1, 100) for _ in range(5)]
     return jsonify(numbers)
 
-app.run(host='0.0.0.0', port=5000, debug=True)
+@limiter.request_filter
+def exempt_users():
+    # You can customize this function to exempt certain users from rate limiting
+    return False
+
+@limiter.request_filter
+def custom_response():
+    # Customize the response when rate limit is exceeded
+    response = make_response(jsonify({"error": "Rate limit exceeded"}), 429)
+    response.headers['Content-Type'] = 'application/json'
+    return response
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
